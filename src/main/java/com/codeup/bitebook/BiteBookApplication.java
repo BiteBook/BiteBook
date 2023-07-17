c22d6890ecb02452028027fa61f543a259262096
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-17 13:02:11 -0400

    adding .gitignore

---
+++ src/main/java/com/codeup/bitebook/BiteBookApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.bitebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/bitebook/BiteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.bitebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
