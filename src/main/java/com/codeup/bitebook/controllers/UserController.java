ad91f16f28447d9df5c9b6e09a763ba6cb9020e7
Joseph Fuentes <josephkfuentes@gmail.com>
2023-07-18 10:53:22 -0400

    Got files started up the right way

---
+++ src/main/java/com/codeup/bitebook/controllers/UserController.java
@@ -0,0 +1,42 @@
+package com.codeup.bitebook.controllers;
+
+import com.codeup.bitebook.models.User;
+import com.codeup.bitebook.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user, Model model) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            model.addAttribute("message", "Username already exists");
+            return "users/sign-up";
+        }
+
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
+
+
---
+++ src/main/java/com/codeup/bitebook/models/User.java
@@ -0,0 +1,40 @@
+package com.codeup.bitebook.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "blog_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "creator")
+    private List<Post> posts;
+
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
+}
