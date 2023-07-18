87a36f11955308df9ef385c4842f05ba1ec21a01
Joseph Fuentes <josephkfuentes@gmail.com>
2023-07-18 12:25:52 -0400

    Don't mess this up

--- src/main/java/com/codeup/bitebook/models/Post.java
+++ src/main/java/com/codeup/bitebook/models/Post.java
@@ -1,6 +1,5 @@
 package com.codeup.bitebook.models;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +12,4 @@ import lombok.Setter;
 public class Post {
     private String title;
     private String body;
-
 }
