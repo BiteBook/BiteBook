337dc0fb0b983e1c14ee995717693b850c71df0e
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-07-19 11:23:58 -0400

    navbar work

--- src/main/java/com/codeup/bitebook/security/SecurityConfiguration.java
+++ src/main/java/com/codeup/bitebook/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.codeup.bitebook.security;
 import com.codeup.bitebook.services.UserDetailsLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
