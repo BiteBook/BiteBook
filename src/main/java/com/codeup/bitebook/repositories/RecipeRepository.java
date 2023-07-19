7dc86d86cc41eca235de96d3387e96e75faf1e75
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-19 12:10:02 -0400

    search filter

--- src/main/java/com/codeup/bitebook/repositories/RecipeRepository.java
+++ src/main/java/com/codeup/bitebook/repositories/RecipeRepository.java
@@ -1,7 +1,16 @@
 package com.codeup.bitebook.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.bitebook.models.Recipe;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    @Query("SELECT r FROM Recipe r WHERE r.title LIKE %?1%"
+    + " OR r.difficulty LIKE %?1%"
+//    + " OR r.time LIKE %?1%"
+    )
+    List<Recipe> findAll(String keyword);
 }
