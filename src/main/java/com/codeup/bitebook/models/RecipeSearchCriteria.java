cb337d2848bfc0972270f8886f79074aa7a4b8af
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-19 17:35:39 -0400

    more changes to searching and filtering (still needs a lot of work)

---
+++ src/main/java/com/codeup/bitebook/models/RecipeSearchCriteria.java
@@ -0,0 +1,14 @@
+package com.codeup.bitebook.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecipeSearchCriteria {
+    private String title;
+    private String region;
+    private String difficulty;
+    private String dietary;
+}
