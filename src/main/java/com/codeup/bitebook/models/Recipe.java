b0fefe525abb4df12a55e46442b8114dd022a13a
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-19 15:20:53 -0400

    dietary added to recipe model

--- src/main/java/com/codeup/bitebook/models/Recipe.java
+++ src/main/java/com/codeup/bitebook/models/Recipe.java
@@ -25,6 +25,7 @@ public class Recipe {
     private String difficulty;
     private Integer time;
     private String region;
+    private String dietary;
 
     @ManyToOne
     private User user;
--- src/main/java/com/codeup/bitebook/repositories/RecipeRepository.java
+++ src/main/java/com/codeup/bitebook/repositories/RecipeRepository.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.bitebook.models.Recipe;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     @Query("SELECT r FROM Recipe r WHERE "
-            + "CONCAT (r.title, r.difficulty, r.region)"
+            + "CONCAT (r.title, r.difficulty, r.region, r.dietary)"
             + " LIKE %?1%")
     List<Recipe> findAll(String keyword);
 }
