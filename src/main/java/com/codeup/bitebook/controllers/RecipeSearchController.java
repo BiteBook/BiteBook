f5ee5095e4d5fd6a1f98eb000dd0f9c6d536c9ab
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-20 12:58:01 -0400

    search bar needs implemented then good to go

--- src/main/java/com/codeup/bitebook/controllers/RecipeSearchController.java
+++ src/main/java/com/codeup/bitebook/controllers/RecipeSearchController.java
@@ -1,34 +1,34 @@
-package com.codeup.bitebook.controllers;
-
-import com.codeup.bitebook.models.Recipe;
-import com.codeup.bitebook.models.RecipePage;
-import com.codeup.bitebook.models.RecipeSearchCriteria;
-import com.codeup.bitebook.services.RecipeService;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
-@RestController
-@RequestMapping("/search")
-public class RecipeSearchController {
-
-    private final RecipeService recipeService;
-
-    public RecipeSearchController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
-
-    @GetMapping
-    public ResponseEntity<Page<Recipe>> getRecipes(RecipePage recipePage,
-                                                   RecipeSearchCriteria recipeSearchCriteria){
-        return new ResponseEntity<>(recipeService.getRecipes(recipePage, recipeSearchCriteria),
-        HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Recipe> addRecipe(Recipe recipe){
-        return new ResponseEntity<>(recipeService.addRecipe(recipe), HttpStatus.OK);
-    }
-}
+//package com.codeup.bitebook.controllers;
+//
+//import com.codeup.bitebook.models.Recipe;
+//import com.codeup.bitebook.models.RecipePage;
+//import com.codeup.bitebook.models.RecipeSearchCriteria;
+//import com.codeup.bitebook.services.RecipeService;
+//import org.springframework.data.domain.Page;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//@RestController
+//@RequestMapping("/search")
+//public class RecipeSearchController {
+//
+//    private final RecipeService recipeService;
+//
+//    public RecipeSearchController(RecipeService recipeService) {
+//        this.recipeService = recipeService;
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<Page<Recipe>> getRecipes(RecipePage recipePage,
+//                                                   RecipeSearchCriteria recipeSearchCriteria){
+//        return new ResponseEntity<>(recipeService.getRecipes(recipePage, recipeSearchCriteria),
+//        HttpStatus.OK);
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<Recipe> addRecipe(Recipe recipe){
+//        return new ResponseEntity<>(recipeService.addRecipe(recipe), HttpStatus.OK);
+//    }
+//}
--- src/main/java/com/codeup/bitebook/repositories/RecipeRepository.java
+++ src/main/java/com/codeup/bitebook/repositories/RecipeRepository.java
@@ -3,9 +3,16 @@ package com.codeup.bitebook.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.bitebook.models.Recipe;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findAllByTitleOrDietaryOrRegionOrDifficultyOrTime(String title, String dietary, String region, String difficulty, Integer time);
+    List<Recipe> findByTitle(String title);
+    List<Recipe> findByDietary(String dietary);
+    List<Recipe> findByRegion(String region);
+    List<Recipe> findByTime(String time);
+    List<Recipe> findByDifficulty(String difficulty);
 
 }
