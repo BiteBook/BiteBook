dfa6fdacd211d6dcdef3e43f6b7b61023e097d46
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-20 16:12:13 -0400

    trying to fix filter and search

--- src/main/java/com/codeup/bitebook/controllers/SearchController.java
+++ src/main/java/com/codeup/bitebook/controllers/SearchController.java
@@ -1,11 +1,17 @@
 package com.codeup.bitebook.controllers;
 
 
+import com.codeup.bitebook.models.Recipe;
 import com.codeup.bitebook.repositories.RecipeRepository;
+import com.codeup.bitebook.services.RecipeServiceAgain;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class SearchController {
@@ -14,35 +20,47 @@ public class SearchController {
     public SearchController(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
-//    @GetMapping("/recipes/all")
-//    public String findByAll(Model model) {
-//        model.addAttribute("recipes", recipeRepository.findAllByTitleOrDietaryOrRegionOrDifficultyOrTime("","","","",0));
-//        return "recipeIndex";
-//    }
-    @GetMapping("/recipe/title")
-    public String searchByTitle(Model model,
-        @Param("keyword") String keyword) {
-        model.addAttribute("recipes", recipeRepository.findByTitle(keyword));
+    @GetMapping("/recipes/all")
+    public String findByAll(Model model) {
+        model.addAttribute("recipes", recipeRepository.findAllByTitleOrDietaryOrRegionOrDifficultyOrTime("","","","",""));
         return "recipeIndex";
     }
-    @GetMapping("/recipe/region")
+    @GetMapping("/recipes/title")
+    public String searchByTitle(Model model) {
+        model.addAttribute("recipes", recipeRepository.findByTitle("Ham"));
+        return "recipeIndex";
+    }
+    @GetMapping("/recipes/region")
     public String searchByRegion(Model model) {
         model.addAttribute("recipes", recipeRepository.findByRegion(""));
         return "recipeIndex";
     }
-    @GetMapping("/recipe/dietary")
+    @GetMapping("/recipes/dietary")
     public String searchByDietary(Model model) {
         model.addAttribute("recipes", recipeRepository.findByDietary(""));
         return "recipeIndex";
     }
-    @GetMapping("/recipe/time")
+    @GetMapping("/recipes/time")
     public String searchByTime(Model model) {
         model.addAttribute("recipes", recipeRepository.findByTime(""));
         return "recipeIndex";
     }
-    @GetMapping("/recipe/difficulty")
+    @GetMapping("/recipes/difficulty")
     public String searchByDifficulty(Model model) {
         model.addAttribute("recipes", recipeRepository.findByDifficulty(""));
         return "recipeIndex";
     }
+
+    @Autowired
+    private RecipeServiceAgain service;
+
+    @RequestMapping("/recipes/searchall")
+    public String viewHomePage(Model model, @Param("keyword") String keyword) {
+        List<Recipe> listRecipes = service.listAll(keyword);
+        model.addAttribute("listRecipes", listRecipes);
+        model.addAttribute("keyword", keyword);
+
+        return "recipeIndex";
+    }
+
 }
