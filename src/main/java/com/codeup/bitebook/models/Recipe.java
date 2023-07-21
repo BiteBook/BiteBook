cbc29048b8e2107dd6e32279aaf6ea8d587b7458
Joseph Fuentes <josephkfuentes@gmail.com>
2023-07-21 16:23:09 -0400

    main original

--- src/main/java/com/codeup/bitebook/models/Recipe.java
+++ src/main/java/com/codeup/bitebook/models/Recipe.java
@@ -18,20 +18,42 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    @Column
     private Long recipeid;
 
+    @Column
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String ingredients;
+    @Column(columnDefinition = "TEXT")
     private String instructions;
+    @Column
     private String tools;
+    @Column
     private String photo;
+    @Column
     private String difficulty;
+    @Column
     private String time;
+    @Column
     private String region;
+    @Column
     private String dietary;
+    @Column
     private Double calories;
+    @Column
     private Double protein;
+    @Column
     private Double fibre;
+    @Column
+    private Double carbohydrates;
+    @Column
+    private Double fats;
+    @Column
+    private Double sugar;
+    @Column
+    private Double sodium;
+
     @OneToMany(mappedBy = "recipe")
     private List<MealPlanner> mealPlanners;
     @ManyToOne
