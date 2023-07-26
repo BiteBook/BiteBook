9d5a83e145b402cebabda311ab0fffafde4075a7
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-07-26 15:14:19 -0400

    image uploading and saving

--- src/main/resources/static/javascript/keys.js
+++ src/main/resources/static/javascript/keys.js
@@ -3,7 +3,7 @@ const APIKEY = 'd145f8770c0e24f240003773c6b6ea40';
 const USERID = '46040ebe';
 
 const APIKEY2  = '43c9ca641fd149cfa982fabb08dfabe7';
-
+const FTKEY = 'AP72YdaNdSv2KE0YsTzHCz'
 
 /*
 
