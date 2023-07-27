91f70493b8197d9552d450f3fa24c7181d270b40
Patrick Mireles <patrickm.mireles@gmail.com>
2023-07-27 10:24:20 -0500

    addedcontroller

--- src/main/java/com/codeup/bitebook/controllers/RatingController.java
+++ src/main/java/com/codeup/bitebook/controllers/RatingController.java
@@ -1,4 +1,50 @@
 package com.codeup.bitebook.controllers;
+import com.codeup.bitebook.models.Recipe;
+import jakarta.persistence.*;
 
-public class RatingController {
+
+@Entity
+@Table(name="recipe-reviews")
+
+public class RatingController  {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    @Column(nullable = false)
+    private String rating;
+
+    @ManyToOne
+    @JoinColumn (name = "recipeid")
+    private Recipe recipe;
 }
+
+
