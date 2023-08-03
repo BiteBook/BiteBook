40f9aef86438550adba93d1e4dd289cbf36767c8
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-08-03 15:59:54 -0400

    carousels move

--- src/main/resources/static/javascript/Slick.js
+++ src/main/resources/static/javascript/Slick.js
@@ -1,17 +1,26 @@
 
-
 var $jq = jQuery.noConflict();
 
 $jq(document).ready(function() {
     $jq('#recipeList').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        autoplay: true, // Enables auto sliding
+        autoplaySpeed: 3500, // Time in milliseconds for each slide (2 seconds)
+        arrows: true, // Enables navigation arrows
+        dots: true, // Enables navigation dots
+        speed: 2000
     });
 
     $jq('#readyList').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        autoplay: true, // Enables auto sliding
+        autoplaySpeed: 3500, // Time in milliseconds for each slide (2 seconds)
+        arrows: true, // Enables navigation arrows
+        dots: true, // Enables navigation dots
+        speed: 2000
     });
 });
